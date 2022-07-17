8fdde3549241140ca3a315621994b53c1ab676ce
mtvzee <sample@aaaaa.com>
2022-07-17 19:02:18 +0900

    ダミーを追加する機能

--- components/AddFriendModal.tsx
+++ components/AddFriendModal.tsx
@@ -1,5 +1,17 @@
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
-import { Dispatch, FormEvent, SetStateAction, useState } from 'react';
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  setDoc,
+} from 'firebase/firestore';
+import {
+  Dispatch,
+  FormEvent,
+  MouseEvent,
+  SetStateAction,
+  useState,
+} from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { AiOutlineClose } from 'react-icons/ai';
 import { auth, db } from '../firebase';
@@ -13,7 +25,9 @@ const AddFriendModal = ({ setIsOpen }: Props) => {
   const [input, setInput] = useState('');
   const [user] = useAuthState(auth);
 
-  const addFriend = async (e: FormEvent<HTMLFormElement>) => {
+  const addFriend = async (
+    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+  ) => {
     e.preventDefault();
     await addDoc(collection(db, 'chats'), {
       latestMessage: '',
@@ -24,31 +38,55 @@ const AddFriendModal = ({ setIsOpen }: Props) => {
     setIsOpen(false);
   };
 
+  const addDummy = async () => {
+    // await addDoc(collection(db, 'chats'), {
+    //   latestMessage: '',
+    //   timestamp: serverTimestamp(),
+    //   users: [user?.email, 'dummypz8@gmail.com'],
+    // });
+    await setDoc(doc(db, `chats/${user?.uid}`), {
+      latestMessage: '',
+      timestamp: serverTimestamp(),
+      users: [user?.email, 'dummypz8@gmail.com'],
+    });
+    await addDoc(collection(db, `chats/${user?.uid}/messages`), {
+      senderEmail: 'dummypz8@gmail.com',
+      photoURL:
+        'https://lh3.googleusercontent.com/a/AItbvmk3WsfgwrkEqwvJPQHMLOtMRUgpHIMF8LOKybqG=s96-c',
+      timestamp: serverTimestamp(),
+      text: 'こんにちは',
+    });
+    setIsOpen(false);
+  };
+
   return (
     <div className={styles.overlay} onClick={() => setIsOpen(false)}>
-      <form
-        className={styles.content}
-        onSubmit={(e) => addFriend(e)}
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className={styles.content} onClick={(e) => e.stopPropagation()}>
         <button className={styles.close_btn} onClick={() => setIsOpen(false)}>
           <AiOutlineClose size={25} />
         </button>
         <h1 className={styles.title}>友達のメールアドレスを入力</h1>
-        <input
-          type="email"
-          className={styles.email}
-          placeholder="メールアドレス"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
+        <div className={styles.input}>
+          <input
+            type="email"
+            className={styles.email}
+            placeholder="メールアドレス"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button className={styles.dummy} onClick={addDummy}>
+            ダミーを追加する
+          </button>
+        </div>
         <div className={styles.footer_btn}>
-          <button className={styles.delete}>追加</button>
+          <button className={styles.delete} onClick={(e) => addFriend(e)}>
+            追加
+          </button>
           <button className={styles.cancel} onClick={() => setIsOpen(false)}>
             キャンセル
           </button>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
